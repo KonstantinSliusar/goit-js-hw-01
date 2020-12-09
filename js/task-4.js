5a5eee078a68e0da53dd58b1dfa3e0436c0a9fde
KonstantinSliusar <61802266+KonstantinSliusar@users.noreply.github.com>
2020-12-09 19:39:04 +0200

    Add files via upload

--- js/task-4.js
+++ js/task-4.js
@@ -9,16 +9,17 @@ if(count === null) {
     result = "Отменено пользователем!";
 } else { 
     totalPrice = pricePerDroid * count;
-}
 
 if((credits - totalPrice) <= 0) {  
     result = "Недостаточно средств на счету!.";
-} else if (credits = credits - totalPrice){
+} else {
+    (credits = credits - totalPrice);
     result = `Вы купили ${count} дроида(ов), на счету осталось ${credits}`;
-    
+    }
 }
 
 console.log(result);
+
     
         
          
